b35439ea7d6bc2226aa8c8d1d1f074511a1e1822
dima1di <66922884+dima1di@users.noreply.github.com>
2021-10-21 17:38:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from bs4 import BeautifulSoup
 import time
 token = '2099132828:AAFFsSEwxR3EaW0akhXoy-WxQzDjB2fvmyA'
 requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Софт работает')
-i = 0
+i = 1
+coin_2 = 1
 while True:
 	response = requests.get('https://coinmarketcap.com/new/')
 	soup = BeautifulSoup(response.content, 'html.parser')
 	coin = soup.find('div', {'class': 'sc-1teo54s-2 fZIJcI'}).text
-	print(coin)
-	if coin == coin:
+	if coin == coin_2:
 		time.sleep(300)
-		if i == 11:
+		if i == 12:
 			requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Софт работает')
-			print(i)
 			i = 0
 		i = i + 1
+		coin_2 = coin
 	else:
+		coin_2 = coin
 		coin = coin[1:]
 		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Новый токен! ' +coin)
