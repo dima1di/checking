315904928e5ea0595fea8246f7864653bff5fb70
dima1di <66922884+dima1di@users.noreply.github.com>
2021-10-21 22:56:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ while True:
 			i = 0
 		i = i + 1
 		coin_2 = coin
-		time.sleep(30)
+		time.sleep(25)
 	else:
 		coin_2 = coin
 		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Новый токен! ' +coin)
