cc68dd6933ab47e307a2b522b973d15118c6a4a0
dima1di <66922884+dima1di@users.noreply.github.com>
2021-10-22 12:36:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import requests 
 from bs4 import BeautifulSoup
 import time
-token = '2099132828:AAFFsSEwxR3EaW0akhXoy-WxQzDjB2fvmyA'
-requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Софт работает')
+token = ''
+requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=&text=Софт работает')
 i = 1
 coin_2 = 1
 while True:
-	requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Просто')
+	requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=&text=Просто')
 	response = requests.get('https://coinmarketcap.com/new/')
 	soup = BeautifulSoup(response.content, 'html.parser')
 	coin = soup.find('div', {'class': 'sc-1teo54s-2 fZIJcI'}).text
 	coin = coin[1:]
 	if coin == coin_2:
-		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=' +coin)
+		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=&text=' +coin)
 		if i == 24:
-			requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Софт работает')
+			requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=&text=Софт работает')
 			i = 0
 		i = i + 1
 		coin_2 = coin
 		time.sleep(15)
 	else:
 		coin_2 = coin
-		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=624064595&text=Новый токен! ' +coin)
+		requests.get('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=&text=Новый токен! ' +coin)
